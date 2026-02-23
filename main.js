b9167254af2529307d390288f3dc4cf6d6e54bbc
Draiphis <val08350@gmail.com>
2026-02-23 14:25:10 +0100

    added fecth json

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const pairOfPokemonIds = ["pikachu", "pikachu", "charmander", "charmander"];
+fetch("http://127.0.0.1:5500/data/pokemon.json")
+    .then(response=>{
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+    })
+
