7c2b649f1ce073bfab85fc4fb7d1861c02627792
Draiphis <val08350@gmail.com>
2026-02-27 14:51:43 +0100

    ca marche presque

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import * as catched from "./modules/pokemoncatched.js";
 const grille_jeu = document.getElementById("grille_de_jeu");
 const divs = grille_jeu.querySelectorAll("div");
 let premierChoix = null;
+let deuxiemeChoix = null;
+let listIndex = [];
+let index1;
+let index2;
 
 let pokemonIds = [];
 let pairShuffled = [];
@@ -26,11 +30,35 @@ fetch("http://127.0.0.1:5500/data/pokemon.json")
 
     divs.forEach((div, index) => {
       div.addEventListener("click", () => {
-        displayPokemon(index);
+        displayPokemon(index, pairShuffled[index]);
+        listIndex.push(index);
+
+        if (!premierChoix || (premierChoix && deuxiemeChoix)) {
+          premierChoix = pairShuffled[index];
+          deuxiemeChoix = null;
+          console.log("premier choix");
+          index1 = index;
+        } else {
+          deuxiemeChoix = pairShuffled[index];
+          index2 = index;
+          console.log("deuxieme Choix");
+
+          if (comparePath(premierChoix, deuxiemeChoix)) {
+            setTimeout(() => {
+              displayPokeball(index1);
+              displayPokeball(index2);
+              premierChoix = null;
+            }, 1000);
+          } else {
+            setTimeout(() => {
+              displayBush(index1);
+              displayBush(index2);
+              premierChoix = null;
+            }, 1000);
+          }
+        }
       });
     });
-
-    return pairShuffled;
   });
 
 function melangerListe(liste) {
@@ -104,12 +132,32 @@ function displayPokeball(box_num) {
   image.classList.remove("bush", "pokemon");
 }
 
-function displayPokemon(box_num) {
+function displayBush(box_num) {
   const box = grille_jeu.children[box_num];
   console.log(box);
   const image = box.firstElementChild;
 
-  image.src = pairShuffled[box_num];
+  image.src = "assets/bush.webp";
+  image.classList.add("bush");
+  image.classList.remove("pokeball", "pokemon");
+}
+
+function displayPokemon(box_num, listPath) {
+  const box = grille_jeu.children[box_num];
+  console.log(box);
+  const image = box.firstElementChild;
+
+  image.src = listPath;
   image.classList.add("pokemon");
   image.classList.remove("bush", "pokeball");
+}
+
+function comparePath(path1, path2) {
+  if (path1 == path2) {
+    console.log("chemin identique");
+    return true;
+  } else {
+    console.log("choix différent");
+    return false;
+  }
 }
